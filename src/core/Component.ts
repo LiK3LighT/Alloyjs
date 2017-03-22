8ca0dcee1fce4dd12ec7e6c8c7505fffc4eed311
Dominik König <koenig.domi@googlemail.com>
2017-03-22 23:28:59 +0100

    fixed a problem where nodes in a shadowRoot were not updated

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -331,7 +331,7 @@ export class Component extends HTMLElement {
                 htmlNodeToUpdate = nodeToUpdate;
             }
 
-            if(document.body.contains(htmlNodeToUpdate) === false) continue; // Skip nodes that are not added to the visible dom, can't use parentElement cause that would be null if the element was in a shadowRoot, CharacterData can apparantly also not be connected which is why the htmlNodeToUpdate === null check exists
+            if(htmlNodeToUpdate.getRootNode() === htmlNodeToUpdate) continue; // Skip nodes that are not added to the visible dom, can't use parentElement cause that would be null if the element was in a shadowRoot, CharacterData can apparantly also not be connected which is why the htmlNodeToUpdate === null check exists
 
             //TODO: Make this a seperate expression e.g. {{this.variable}} and remove value[2] from the bindMap
             // LoopFor through variables, check if of type node and evaluate them seperately (These variables get removed from the evalText)
